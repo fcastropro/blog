c5b44a4eaa830e55c0d6d7a6230f2a4dbec96cd6
Fernando Castro <fcastropro@gmail.com>
2025-05-28 21:59:35 -0500

    h

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'), // ✅ aquí la solución real
+      secretOrKey: 'superstart', // ✅ aquí la solución real
     });
   }
 
