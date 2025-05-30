3151a344e6b7c6948e45e7666218ebbb5d35795c
Fernando Castro <fcastropro@gmail.com>
2025-05-29 21:21:45 -0500

    cambiamos

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Elian Muriel Programacion III!';
+    return 'Programacion III!';
   }
 }
